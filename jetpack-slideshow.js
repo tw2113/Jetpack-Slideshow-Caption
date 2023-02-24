1e432f6f15aa5c62b607e4d92f9518c28fcec004
Michael Beckwith <tw2113@gmail.com>
2023-02-24 17:24:21 -0600

    update css and js to match current jetpack but retain original edits for plugin purpose

--- jetpack-slideshow.js
+++ jetpack-slideshow.js
@@ -1,29 +1,21 @@
-function JetpackSlideshow( element, width, height, transition ) {
+/* global jetpackSlideshowSettings, escape */
+
+function JetpackSlideshow( element, transition, autostart ) {
 	this.element = element;
 	this.images = [];
 	this.controls = {};
 	this.transition = transition || 'fade';
-
-	var currentWidth = this.element.width();
-	if ( !width || width > currentWidth )
-		width = currentWidth;
-
-	this.width = width;
-	this.height = height;
-	this.element.css( {
-		'height': this.height + 'px'
-		} );
+	this.autostart = autostart;
 }
 
-JetpackSlideshow.prototype.showLoadingImage = function( toggle ) {
+JetpackSlideshow.prototype.showLoadingImage = function ( toggle ) {
 	if ( toggle ) {
 		this.loadingImage_ = document.createElement( 'div' );
-		this.loadingImage_.className = 'slideshow-loading';
+		this.loadingImage_.className = 'jetpack-slideshow-loading';
 		var img = document.createElement( 'img' );
 		img.src = jetpackSlideshowSettings.spinner;
 		this.loadingImage_.appendChild( img );
 		this.loadingImage_.appendChild( this.makeZeroWidthSpan() );
-		this.loadingImage_.style.lineHeight = this.height + 'px';
 		this.element.append( this.loadingImage_ );
 	} else if ( this.loadingImage_ ) {
 		this.loadingImage_.parentNode.removeChild( this.loadingImage_ );
@@ -31,117 +23,143 @@ JetpackSlideshow.prototype.showLoadingImage = function( toggle ) {
 	}
 };
 
-JetpackSlideshow.prototype.init = function() {
-	this.showLoadingImage(true);
+JetpackSlideshow.prototype.init = function () {
+	this.showLoadingImage( true );
 
 	var self = this;
 	// Set up DOM.
 	for ( var i = 0; i < this.images.length; i++ ) {
-		var imageInfo = this.images[i];
+		var imageInfo = this.images[ i ];
 		var img = document.createElement( 'img' );
-		img.src = imageInfo.src + '?w=' + this.width;
+		img.src = imageInfo.src;
+		img.title = typeof imageInfo.title !== 'undefined' ? imageInfo.title : '';
+		img.alt = typeof imageInfo.alt !== 'undefined' ? imageInfo.alt : '';
 		img.align = 'middle';
+		img.setAttribute( 'itemprop', 'image' );
+		img.nopin = 'nopin';
 		var caption = document.createElement( 'div' );
-		caption.className = 'slideshow-slide-caption';
+		caption.className = 'jetpack-slideshow-slide-caption';
+		caption.setAttribute( 'itemprop', 'caption description' );
 		caption.innerHTML = imageInfo.caption;
-		var container = document.createElement('div');
-		container.className = 'slideshow-slide';
-		// container.style.lineHeight = this.height + 'px';
+		var container = document.createElement( 'div' );
+		container.className = 'jetpack-slideshow-slide';
+		container.setAttribute( 'itemprop', 'associatedMedia' );
+		container.setAttribute( 'itemscope', '' );
+		container.setAttribute( 'itemtype', 'https://schema.org/ImageObject' );
 
 		// Hide loading image once first image has loaded.
-		if ( i == 0 ) {
+		if ( i === 0 ) {
 			if ( img.complete ) {
 				// IE, image in cache
-				setTimeout( function() {
+				setTimeout( function () {
 					self.finishInit_();
-				}, 1);
+				}, 1 );
 			} else {
-				jQuery( img ).load(function() {
+				jQuery( img ).load( function () {
 					self.finishInit_();
-				});
+				} );
 			}
 		}
 		container.appendChild( img );
 		// I'm not sure where these were coming from, but IE adds
 		// bad values for width/height for portrait-mode images
-		img.removeAttribute('width');
-		img.removeAttribute('height');
+		img.removeAttribute( 'width' );
+		img.removeAttribute( 'height' );
 		container.appendChild( this.makeZeroWidthSpan() );
 		container.appendChild( caption );
 		this.element.append( container );
 	}
 };
 
-JetpackSlideshow.prototype.makeZeroWidthSpan = function() {
+JetpackSlideshow.prototype.makeZeroWidthSpan = function () {
 	var emptySpan = document.createElement( 'span' );
-	emptySpan.className = 'slideshow-line-height-hack';
+	emptySpan.className = 'jetpack-slideshow-line-height-hack';
 	// Having a NBSP makes IE act weird during transitions, but other
 	// browsers ignore a text node with a space in it as whitespace.
-	if (-1 !== window.navigator.userAgent.indexOf('MSIE ')) {
-		emptySpan.appendChild( document.createTextNode(' ') );
+	if ( -1 !== window.navigator.userAgent.indexOf( 'MSIE ' ) ) {
+		emptySpan.appendChild( document.createTextNode( ' ' ) );
 	} else {
 		emptySpan.innerHTML = '&nbsp;';
 	}
 	return emptySpan;
 };
 
-JetpackSlideshow.prototype.finishInit_ = function() {
+JetpackSlideshow.prototype.finishInit_ = function () {
 	this.showLoadingImage( false );
 	this.renderControls_();
 
 	var self = this;
-	// Initialize Cycle instance.
-	this.element.cycle( {
-		fx: this.transition,
-		prev: this.controls.prev,
-		next: this.controls.next,
-		slideExpr: '.slideshow-slide',
-		before: this.onCycleBefore,
-		onPrevNextEvent: function() {
-			return self.onCyclePrevNextClick_.apply( self, arguments );
-		}
-	} );
+	if ( this.images.length > 1 ) {
+		// Initialize Cycle instance.
+		this.element.cycle( {
+			fx             : this.transition,
+			prev           : this.controls.prev,
+			next           : this.controls.next,
+			timeout        : jetpackSlideshowSettings.speed,
+			slideExpr      : '.jetpack-slideshow-slide',
+			before         : this.onCycleBefore,
+			onPrevNextEvent: function () {
+				return self.onCyclePrevNextClick_.apply( self, arguments );
+			},
+		} );
 
-	var slideshow = this.element;
-	jQuery( this.controls['stop'] ).click( function() {
-		var button = jQuery(this);
-		if ( ! button.hasClass( 'paused' ) ) {
+		var slideshow = this.element;
+
+		if ( ! this.autostart ) {
 			slideshow.cycle( 'pause' );
-			button.removeClass( 'running' );
-			button.addClass( 'paused' );
-		} else {
-			button.addClass( 'running' );
-			button.removeClass( 'paused' );
-			slideshow.cycle( 'resume', true );
+			jQuery( this.controls.stop ).removeClass( 'running' );
+			jQuery( this.controls.stop ).addClass( 'paused' );
 		}
-		return false;
-	} );
 
-	var controls = jQuery( this.controlsDiv_ );
-	slideshow.mouseenter( function() {
-		controls.fadeIn();
-	} );
-	slideshow.mouseleave( function() {
-		controls.fadeOut();
-	} );
+		jQuery( this.controls.stop ).click( function () {
+			var button = jQuery( this );
+			if ( ! button.hasClass( 'paused' ) ) {
+				slideshow.cycle( 'pause' );
+				button.removeClass( 'running' );
+				button.addClass( 'paused' );
+			} else {
+				button.addClass( 'running' );
+				button.removeClass( 'paused' );
+				slideshow.cycle( 'resume', true );
+			}
+			return false;
+		} );
 
+		var controls = jQuery(this.controlsDiv_);
+		slideshow.mouseenter(function () {
+			controls.fadeIn();
+		});
+		slideshow.mouseleave(function () {
+			controls.fadeOut();
+		});
+	} else {
+		this.element.children( ':first' ).show();
+		this.element.css( 'position', 'relative' );
+	}
 	this.initialized_ = true;
 };
 
-JetpackSlideshow.prototype.renderControls_ = function() {
-	if ( this.controlsDiv_ )
+JetpackSlideshow.prototype.renderControls_ = function () {
+	if ( this.controlsDiv_ ) {
 		return;
+	}
 
 	var controlsDiv = document.createElement( 'div' );
-	controlsDiv.className = 'slideshow-controls';
+	controlsDiv.className = 'jetpack-slideshow-controls';
 
-	controls = [ 'prev', 'stop', 'next' ];
+	var controls = [ 'prev', 'stop', 'next' ];
 	for ( var i = 0; i < controls.length; i++ ) {
-		var controlName = controls[i];
+		var controlName = controls[ i ];
+		var label_name = 'label_' + controlName;
 		var a = document.createElement( 'a' );
+
 		a.href = '#';
+		a.className = 'button-' + controlName;
+		a.setAttribute( 'aria-label', jetpackSlideshowSettings[ label_name ] );
+		a.setAttribute( 'role', 'button' );
+
 		controlsDiv.appendChild( a );
-		this.controls[controlName] = a;
+		this.controls[ controlName ] = a;
 	}
 	this.element.append( controlsDiv );
 	this.controlsDiv_ = controlsDiv;
@@ -153,7 +171,7 @@ JetpackSlideshow.prototype.onCycleBefore = function( currSlideElement, nextSlide
 
 	setTimeout( function() {
 		imgheight = jQuery('img', self).outerHeight();
-		captionheight = jQuery('.slideshow-slide-caption',self).outerHeight();
+		captionheight = jQuery('.jetpack-slideshow-slide-caption',self).outerHeight();
 		captionheight = captionheight > 0 ? captionheight + 12 : 0;
 		new_height = imgheight + captionheight;
 
@@ -162,22 +180,30 @@ JetpackSlideshow.prototype.onCycleBefore = function( currSlideElement, nextSlide
 	}, 200);
 }
 
-JetpackSlideshow.prototype.onCyclePrevNextClick_ = function( isNext, i, slideElement ) {
+JetpackSlideshow.prototype.onCyclePrevNextClick_ = function ( isNext, i /*, slideElement*/ ) {
 	// If blog_id not present don't track page views
-	if ( ! jetpackSlideshowSettings.blog_id )
+	if ( ! jetpackSlideshowSettings.blog_id ) {
 		return;
+	}
 
-	var postid = this.images[i].id;
+	var postid = this.images[ i ].id;
 	var stats = new Image();
-	stats.src = document.location.protocol +
-		'//stats.wordpress.com/g.gif?host=' +
+	stats.src =
+		document.location.protocol +
+		'//pixel.wp.com/g.gif?host=' +
 		escape( document.location.host ) +
-		'&rand=' + Math.random() +
-		'&blog=' + jetpackSlideshowSettings.blog_id +
-		'&subd=' + jetpackSlideshowSettings.blog_subdomain +
-		'&user_id=' + jetpackSlideshowSettings.user_id +
-		'&post=' + postid +
-		'&ref=' + escape( document.location );
+		'&rand=' +
+		Math.random() +
+		'&blog=' +
+		jetpackSlideshowSettings.blog_id +
+		'&subd=' +
+		jetpackSlideshowSettings.blog_subdomain +
+		'&user_id=' +
+		jetpackSlideshowSettings.user_id +
+		'&post=' +
+		postid +
+		'&ref=' +
+		escape( document.location );
 };
 
 ( function ( $ ) {
@@ -187,10 +213,15 @@ JetpackSlideshow.prototype.onCyclePrevNextClick_ = function( isNext, i, slideEle
 		$( '.jetpack-slideshow' ).each( function () {
 			var container = $( this );
 
-			if ( container.data( 'processed' ) )
+			if ( container.data( 'processed' ) ) {
 				return;
+			}
 
-			var slideshow = new JetpackSlideshow( container, container.data( 'width' ), container.data( 'height' ), container.data( 'trans' ) );
+			var slideshow = new JetpackSlideshow(
+				container,
+				container.data( 'trans' ),
+				container.data( 'autostart' )
+			);
 			slideshow.images = container.data( 'gallery' );
 			slideshow.init();
 
